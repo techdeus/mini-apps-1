7b78ff736578dfc24d6d05eaf7ba37baecbacabd
techdeus <sullivan.marlon@gmail.com>
2018-10-15 19:35:12 -0700

    Finish basic app with all requirements

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,21 @@
 
 let flag = 1;
 let pieces = {
-	X: false,
-	O: false
-}
-let gameOver = false;
+	X: 
+		{
+		status: false,
+		wins: 0
+		},
+	O: 
+		{
+		status: false,
+		wins: 0
+		},
+};
 
+let ties = 0;
+let gameOver = false;
+let moves = 0;
 var elements = document.querySelectorAll("td");
 var turn = document.getElementById('gameMessage');
 
@@ -16,11 +26,13 @@ elements.forEach( (node) => {
 			node.innerHTML = 'X';
 			gameMessage.innerHTML = 'O\'' + 's Turn';
 			flag = 0;
+			moves++;
 			checkWinner();
 		} else if (node.innerHTML === ' ' && !gameOver) {
 			node.innerHTML = 'O';
 			gameMessage.innerHTML = 'X\'' + 's Turn';
 			flag = 1;
+			moves++;
 			checkWinner();
 		}
 	});
@@ -34,29 +46,34 @@ let clearGame = () => {
 		}
 	}
 	flag = 1;
-	pieces.X = false;
-	pieces.O = false;
+	pieces.X.status = false;
+	pieces.O.status = false;
 	gameOver = false;
+	resetMoves();
 	gameMessage.innerHTML = 'Start a new game: X\'' + 's Turn';
-}
+};
 
-let checkAllRows = () => {
+const checkAllRows = () => {
 	var rows = document.getElementById("board").getElementsByTagName("tr");
 	// console.log(rows);
 	for (var i = 0; i < rows.length; i++) {
 		if ( checkRow(rows[i].children) ) {
-			if (pieces.X) {
+			if (pieces.X.status) {
 				gameMessage.innerHTML = "X Wins";
+				updateScoreboard();
+				resetMoves();
 				gameOver = true;
 			} else {
 				gameMessage.innerHTML = "O Wins";
+				updateScoreboard();
+				resetMoves();
 				gameOver = true;
 			}
 		}
 	};
-} 
+}; 
 
-let checkRow = (row) => {
+const checkRow = (row) => {
 	var x = 0;
 	var o = 0;
 	// loop through rows
@@ -68,17 +85,19 @@ let checkRow = (row) => {
 		}
 	}
 	if (x === 3) { 
-		pieces.X = true;
+		pieces.X.status = true;
+		pieces.X.wins++;
 		return true; 
 	} else if (o === 3) {
-		pieces.O = true;
+		pieces.O.status = true;
+		pieces.O.wins++;
 		return true;
 	} else {
 		return false;
 	}
-}
+};
 
-let checkAllColumns = () => {
+const checkAllColumns = () => {
 	var columns = document.getElementById("board").getElementsByTagName("td");
 	var array = [];
 	
@@ -88,20 +107,27 @@ let checkAllColumns = () => {
 	if (array[0] + array[3] + array[6] === 264 || 
 		array[1] + array[4] + array[7] === 264 || 
 		array[2] + array[5] + array[8] === 264) {
-			pieces.X = true;
+			pieces.X.status = true;
+			pieces.X.wins++;
+			updateScoreboard();
+			resetMoves();
 			gameMessage.innerHTML = "X Wins";
+			moves = 0;
 			gameOver = true;
 			
 	} else if (array[0] + array[3] + array[6] === 237 || 
 		array[1] + array[4] + array[7] === 237 || 
 		array[2] + array[5] + array[8] === 237) {
-			pieces.O = true;
+			pieces.O.status = true;
+			pieces.O.wins++;
+			updateScoreboard();
+			resetMoves();
 			gameMessage.innerHTML = "O Wins"
 			gameOver = true;
 	}
-}
+};
 
-let checkAllDiagonals = () => {
+const checkAllDiagonals = () => {
 	var columns = document.getElementById("board").getElementsByTagName("td");
 	var array = [];
 
@@ -111,21 +137,49 @@ let checkAllDiagonals = () => {
 
 	if (array[0] + array[4] + array[8] === 264 || 
 		array[2] + array[4] + array[6] === 264) {
-		pieces.X = true;
+		pieces.X.status = true;
+		pieces.X.wins++;
+		updateScoreboard();
+		resetMoves();
 		gameMessage.innerHTML = "X Wins";
 		gameOver = true;
 	} else if (array[0] + array[4] + array[8] === 237 || 
 		array[2] + array[4] + array[6] === 237) {
-		pieces.O = true;
+		pieces.O.status = true;
+		pieces.O.wins++;
+		updateScoreboard();
+		resetMoves();
 		gameMessage.innerHTML = "O Wins";
 		gameOver = true;
 	}
-}
+};
 
-let checkWinner = () => {
+const tieGame = () => {
+	if (moves === 9) {
+		gameMessage.innerHTML = "The game is tied! Play again!";
+		ties++;
+		updateScoreboard();
+		resetMoves();
+	}
+};
+
+
+const checkWinner = () => {
 	checkAllRows();
 	checkAllColumns();
 	checkAllDiagonals();
-}
+	tieGame();
+};
+
+const resetMoves = () => {
+	moves = 0;
+};
+
+const updateScoreboard = () => {
+	document.getElementById('XCount').innerHTML = String(pieces.X.wins);
+	document.getElementById('OCount').innerHTML = String(pieces.O.wins);
+	document.getElementById('Ties').innerHTML = String(ties);
+
+};
 
 
